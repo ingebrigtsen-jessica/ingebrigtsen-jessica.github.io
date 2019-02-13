5fe05e12075b928caa3212b661bc64c631845856
Jessica Ingebrigtsen <aron.jess@gmail.com>
2019-02-13 11:20:23 -0800

    manip text

---
+++ csswithjs/scripts/changeText.js
@@ -0,0 +1,22 @@
+function changeColor() {
+document.getElementById("invite").style.color = "#C5F5F0";
+document.getElementById("location1").style.color = "#f5c5ca";
+document.getElementById("bridegroom").style.color = "#f1fdfb";
+document.getElementById("location2").style.color = "#f1fdfb";
+
+}
+
+function changeFont() {
+    document.getElementById("invite").style.fontFamily = "'Noticia Text', serif";
+    document.getElementById("location1").style.fontFamily = "'Noticia Text', serif";
+    document.getElementById("bridegroom").style.fontFamily = "'Dancing Script', cursive";
+    document.getElementById("location2").style.fontFamily = "'Dancing Script', cursive";
+    
+}
+
+function changeSize() {
+    document.getElementById("invite").style.fontSize = ".15rem";
+    document.getElementById("location1").style.fontSize = ".15rem";
+    document.getElementById("bridegroom").style.fontSize = "2rem";
+    document.getElementById("location2").style.fontSize = "2rem";
+}
