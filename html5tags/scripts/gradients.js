ce47eaf3f683d408a3c55f087f54677937270577
Jessica Ingebrigtsen <aron.jess@gmail.com>
2019-02-12 16:12:48 -0800

    radial gradients

--- html5tags/scripts/gradients.js
+++ html5tags/scripts/gradients.js
@@ -15,3 +15,28 @@ function colorLinear() {
     //draw a rectangle
     ctx.fillRect(30,30,150,100);
 }
+
+function colorRadial() {
+
+    let canvas = document.getElementById("colorhere");
+    let ctx = canvas.getContext("2d");
+
+    //(x start, y start, radius start, x end, y end, radius end)
+    let gradient = (ctx.createRadialGradient(75,50,5,90,60,100))
+
+    //a value betwen 0.0 and 1.0 represents position between start and end in gradient
+    gradient.addColorStop(0, "#f5c5ca");
+    gradient.addColorStop(1,"#C5F5F0");
+
+    //make fillstyle the new gradient (for browser)
+    ctx.fillStyle = gradient;
+
+    //draw a rectangle
+    ctx.fillRect(30,30,150,100);
+}
+
+function clearColors() {
+    let canvas = document.getElementById("colorhere");
+    let ctx = canvas.getContext('2d');
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+}
