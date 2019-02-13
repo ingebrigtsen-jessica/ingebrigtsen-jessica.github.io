6dec9d412f59f3bebc859c0bfbcc05658b76ac10
Jessica Ingebrigtsen <aron.jess@gmail.com>
2019-02-12 22:04:51 -0800

    volume

--- html5tags/scripts/volume.js
+++ html5tags/scripts/volume.js
@@ -3,3 +3,9 @@ function changeVolume(x){
     let video = document.getElementById("spirit2");
     video.volume = x / 100;
 }
+
+function changeVolume(x){
+
+    let video = document.getElementById("rejoice");
+    video.volume = x / 100;
+}
