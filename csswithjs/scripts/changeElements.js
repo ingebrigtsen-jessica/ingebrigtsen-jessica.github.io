d9ff934774faa24d4bb6392917d595e5d8084179
Jessica Ingebrigtsen <aron.jess@gmail.com>
2019-02-13 16:35:26 -0800

    pictures

--- csswithjs/scripts/changeElements.js
+++ csswithjs/scripts/changeElements.js
@@ -1,25 +1,30 @@
 function changeBackground() {
 
-let myElements = document.querySelectorAll("span");
+    let myElements = document.querySelectorAll("span");
 
-for (let i = 0; i < myElements.length; i++) {
-	myElements[i].style.color = "#f5c5ca";
-}
+    for (let i = 0; i < myElements.length; i++) {
+        myElements[i].style.color = "#f5c5ca";
+    }
 }
 
 function changeSpanFont() {
 
     let myElements = document.querySelectorAll("span");
-    
+
     for (let i = 0; i < myElements.length; i++) {
         myElements[i].style.fontFamily = "'Dancing Script', cursive";
     }
-    }
+}
 
-    function changeWithSelectors() {
-        let myElements = document.querySelectorAll("span");
-        myElements[4].style.backgroundColor = "#f1fdfb";
-    
+function changeWithSelectors() {
+    let element = document.querySelectorAll("button");
+    console.log(element[6].style.backgroundColor);
+
+    if (element[6].style.backgroundColor == "rgb(245, 197, 202)")
+        element[6].style.backgroundColor = "#C5F5F0"
+    else
+        element[6].style.backgroundColor = "#f5c5ca";
 
 
-    }
+
+}
---
+++ csswithjs/scripts/changePictures.js
@@ -0,0 +1,5 @@
+function changePicture(amount){
+    let picture = document.getElementById("adjusting");
+
+    picture.style.width = amount + "%";
+}
