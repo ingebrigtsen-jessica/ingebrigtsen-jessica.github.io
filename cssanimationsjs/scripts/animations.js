d37dd2dedc2bacd0a5839f586262c2d3ddbc3b31
Jessica Ingebrigtsen <aron.jess@gmail.com>
2019-02-20 15:56:26 -0800

    move

--- cssanimationsjs/scripts/animations.js
+++ cssanimationsjs/scripts/animations.js
@@ -14,3 +14,22 @@ else {
 triangle.classList.toggle("rotate");
 
 }
+
+function moving() {
+    let circle = document.getElementsByClassName('box5')[0];
+    let button = document.getElementsByClassName('movebtn')[0];
+
+    if(button.innerHTML == "Move"){
+        button.innerHTML = "Pause";
+        circle.classList.add("walkAbout");
+        circle.classList.remove("pauseWalk");
+    }else {
+        button.innerHTML = "Move";
+        circle.classList.add("pauseWalk");
+    }
+}
+
+function reset() {
+    let circle = document.getElementsByClassName('box5')[0];
+    circle.classList.remove("walkAbout");
+}
