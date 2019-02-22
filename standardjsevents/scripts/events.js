31cc82f61e2b6075406e05b9a7112544ffc75100
Jessica Ingebrigtsen <aron.jess@gmail.com>
2019-02-21 21:22:09 -0800

    mouseover

--- standardjsevents/scripts/events.js
+++ standardjsevents/scripts/events.js
@@ -1,15 +1,34 @@
+//mouse over
+document.getElementById('pink').addEventListener("mouseover", function() {
+    changeColor(1);
+});
+document.getElementById('blue').addEventListener("mouseover", function() {
+    changeColor(2);
+});
+document.getElementById('white').addEventListener("mouseover", function() {
+    changeColor(3);
+});
+
+//mouse out
+document.getElementById('pink').addEventListener("mouseout", mouseOut);
+document.getElementById('blue').addEventListener("mouseout", mouseOut);
+document.getElementById('white').addEventListener("mouseout", mouseOut);
+
 function changeColor(color) {
 
     let results = document.getElementsByClassName('results')[0];
 
     switch (color) {
         case "pink":
+        case 1:
             results.style.background = "#f5c5ca";
             break;
         case "blue":
+        case 2:
             results.style.background = "#C5F5F0";
             break;
         case "white":
+        case 3:
             results.style.background = "#f1fdfb";
             break;
         default:
@@ -17,4 +36,11 @@ function changeColor(color) {
             break;
 
     }
+}
+
+
+function mouseOut(){
+    let results = document.getElementsByClassName('results')[0];
+    results.style.background = "transparent";
+
 }
