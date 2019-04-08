0a9714e51151bd486d3d10ac1d8b731716573d29
Jessica Ingebrigtsen <aron.jess@gmail.com>
2019-04-08 13:25:08 -0700

    menu bar

---
+++ finalproject/scripts/hamburger.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+   let x = document.getElementById("menuLinks");
+   if (x.style.display === "block"){
+       x.style.display = "none";
+   } else {
+       x.style.display = "block";
+   }
+}
